f5aac5169462376c311a9d4a82487ba8ecf566b9
NotSandy <sliu9982@gmail.com>
2022-01-02 20:25:11 -0500

    feat(chart): create chart component

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
+import { useState } from 'react/cjs/react.development'
 
 const NewExpense = (props) => {
+
+    const [isEditing, setIsEditing] = useState(false);
 
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
@@ -10,10 +13,22 @@ const NewExpense = (props) => {
             id: Math.random().toString()
         }
         props.onAddExpense(expenseData);
+        setIsEditing(false);
     }
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    }
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    }
+
+    const addExpenseButton = <button onClick={startEditingHandler}>Add New Expense</button>
+
     return (
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+            {!isEditing ? addExpenseButton : <ExpenseForm onCancel={stopEditingHandler} onSaveExpenseData={saveExpenseDataHandler}/>}
         </div>
     )
 }
